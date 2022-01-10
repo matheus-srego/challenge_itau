06c49e1f69aca9fb5c7f28fb976d5006b569bf68
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-01-09 23:27:11 -0300

    Criando projeto base

---
+++ FieldsValidation/FieldsValidation/src/main/java/com/challange/itidigital/fieldsvalidation/FieldsValidationApplication.java
@@ -0,0 +1,13 @@
+package com.challange.itidigital.fieldsvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldsValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldsValidationApplication.class, args);
+	}
+
+}
---
+++ FieldsValidation/FieldsValidation/src/test/java/com/challange/itidigital/fieldsvalidation/FieldsValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challange.itidigital.fieldsvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldsValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
