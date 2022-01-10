6aa9e4a96d609fd8ab8bdc76091dc6e020587534
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-01-10 20:20:56 -0300

    Criando factory a fim de gerenciar as implementações dos serviços

--- FieldsValidation/src/main/java/com/challange/itidigital/fieldsvalidation/services/ValidatorFactory.java
+++ FieldsValidation/src/main/java/com/challange/itidigital/fieldsvalidation/services/ValidatorFactory.java
@@ -1,13 +1,43 @@
 package com.challange.itidigital.fieldsvalidation.services;
 
+import com.challange.itidigital.fieldsvalidation.servicesimpl.ValidatorBlankSpaceServiceImpl;
+import com.challange.itidigital.fieldsvalidation.servicesimpl.ValidatorDigitServiceImpl;
+import com.challange.itidigital.fieldsvalidation.servicesimpl.ValidatorLowercaseLetterServiceImpl;
 import com.challange.itidigital.fieldsvalidation.servicesimpl.ValidatorNumberOfCharactersServiceImpl;
+import com.challange.itidigital.fieldsvalidation.servicesimpl.ValidatorSpecialCharacterServiceImpl;
+import com.challange.itidigital.fieldsvalidation.servicesimpl.ValidatorUppercaseLetterServiceImpl;
 
 public class ValidatorFactory {
 	
 	public static ValidatorService getValidatorType( String name ) {
 		switch( name ) {
-			case "hasNumberOfharactersValid":
+			case "hasNumberOfCharactersValid":
 				return new ValidatorNumberOfCharactersServiceImpl();
+				
+			case "hasDigit":
+				return new ValidatorDigitServiceImpl();
+				
+			case "hasLowercaseLetter":
+				return new ValidatorLowercaseLetterServiceImpl();
+				
+			case "hasUppercaseLetter":
+				return new ValidatorUppercaseLetterServiceImpl();
+				
+			case "hasSpecialCharacter":
+				return new ValidatorSpecialCharacterServiceImpl();
+				
+//			case "hasRepeatedLowercaseLetter":
+//				return new ValidatorRepeatedLowercaseLetterServiceImpl();
+				
+//			case "hasRepeatedUppercaseLetter":
+//				return new ValidatorRepeatedUppercaseLetterServiceImpl();
+				
+//			case "hasRepeatedSpecialCharacter":
+//				return new ValidatorRepeatedSpecialCharacterServiceImpl();
+				
+			case "noExistBlankSpace":
+				return new ValidatorBlankSpaceServiceImpl();
+				
 			default: 
 				return null;
 		}
