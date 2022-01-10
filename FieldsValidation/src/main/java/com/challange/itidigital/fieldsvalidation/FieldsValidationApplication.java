e3081004b0bfc7571d2ea364ea7e42ed100e4478
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-01-09 23:43:51 -0300

    Arquivos criados na compilação do projeto

--- FieldsValidation/src/main/java/com/challange/itidigital/fieldsvalidation/FieldsValidationApplication.java
+++ FieldsValidation/src/main/java/com/challange/itidigital/fieldsvalidation/FieldsValidationApplication.java
@@ -2,12 +2,22 @@ package com.challange.itidigital.fieldsvalidation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FieldsValidationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FieldsValidationApplication.class, args);
+	}
+	
+	@GetMapping
+	@ResponseBody
+	public String HelloDocker() {
+		return "Docker Started!";
 	}
 
 }
