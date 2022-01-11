58789fc36a36183ee322bc0e4c8d6a93f58b7b09
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-01-11 20:45:27 -0300

    Criando testes para o método isValidPassword

--- FieldsValidation/src/test/java/com/challenge/itidigital/fieldsvalidation/FieldsValidationApplicationTests.java
+++ FieldsValidation/src/test/java/com/challenge/itidigital/fieldsvalidation/FieldsValidationApplicationTests.java
@@ -14,15 +14,17 @@ import com.challenge.itidigital.fieldsvalidation.services.ValidatorFieldService;
 class FieldsValidationApplicationTests {
 	
 	@ParameterizedTest
-	@ValueSource( strings = {"AbTp9!fok"} )
-	public void validatePassword( String password ) {
+	@ValueSource( strings = {"@ozHaR0Q9", "3uEAR60t7Po%fMcg8Gi", "AbTp9!fok", "Ob&)ZsmKq(QkWCg^2U9-M0N", "q3O!s)Qf+"} )
+	public void validateIsPassword( String password ) {
 		
 		assertTrue( validatorFieldService.isPasswordValid( password ) );
 		
 	}
 	
 	@ParameterizedTest
-	@ValueSource( strings = {" ", "aa", "ab", "AAAbbbCc", "AbTp9!foo", "AbTp9!foA", "AbTp9 fok"} )
+	@ValueSource( strings = {"x0AA 1_nUVz", "", "S5B#Z4G4!8)D", "PsL-xCaY", "NzXIW-M15M", 
+							 "5+g2!b6N5", "HEa!wuz5!", "zntUX2YGhjjjC#I!", "AO(z)00+Vh4O-oP", 
+							 "Slt7j-(OUA!)+ppt86$3", "K^Dpn@@eE9", "YY5R8Yfw$AW", "hJLhdb6K9CmDnAe", "@0wrmz6q7d9up"} )
 	public void passowrdIsNotValid( String password ) {
 		
 		assertFalse( validatorFieldService.isPasswordValid( password ) );
