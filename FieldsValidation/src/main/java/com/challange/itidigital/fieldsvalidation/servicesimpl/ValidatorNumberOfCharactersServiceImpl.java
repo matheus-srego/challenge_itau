33b4adacbd7772cb962492fe8bc7e078f4bc7723
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2022-01-11 00:51:20 -0300

    Adicionando número hardcode

--- FieldsValidation/src/main/java/com/challange/itidigital/fieldsvalidation/servicesimpl/ValidatorNumberOfCharactersServiceImpl.java
+++ FieldsValidation/src/main/java/com/challange/itidigital/fieldsvalidation/servicesimpl/ValidatorNumberOfCharactersServiceImpl.java
@@ -1,18 +1,13 @@
 package com.challange.itidigital.fieldsvalidation.servicesimpl;
 
-import org.springframework.beans.factory.annotation.Value;
-
 import com.challange.itidigital.fieldsvalidation.services.ValidatorService;
 
 public class ValidatorNumberOfCharactersServiceImpl implements ValidatorService {
 	
-	@Value( "9" )
-	private static int minimunNumberOfCharacters;
-	
 	@Override
 	public Boolean validate( String field ) {
 		final int quantityOfCharacters = field.length();
-		final Boolean quantityIsValid = quantityOfCharacters >= minimunNumberOfCharacters; 
+		final Boolean quantityIsValid = quantityOfCharacters >= 9; 
 		
 		return quantityIsValid;
 	}
