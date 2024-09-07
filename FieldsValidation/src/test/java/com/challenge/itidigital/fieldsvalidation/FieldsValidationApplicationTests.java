5060eeea83153a95b6a0f12714903c684c2b552e
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-09-07 14:47:35 -0300

    style(tests): format code

--- FieldsValidation/src/test/java/com/challenge/itidigital/fieldsvalidation/FieldsValidationApplicationTests.java
+++ FieldsValidation/src/test/java/com/challenge/itidigital/fieldsvalidation/FieldsValidationApplicationTests.java
@@ -12,9 +12,15 @@ import com.challenge.itidigital.fieldsvalidation.services.ValidatorFieldService;
 
 @SpringBootTest
 class FieldsValidationApplicationTests {
-	
+
+	@Autowired
+	private ValidatorFieldService validatorFieldService;
+
 	@ParameterizedTest
-	@ValueSource( strings = {"@ozHaR0Q9", "3uEAR60t7Po%fMcg8Gi", "AbTp9!fok", "Ob&)ZsmKq(QkWCg^2U9-M0N", "q3O!s)Qf+"} )
+	@ValueSource( strings = {
+			"@ozHaR0Q9", "3uEAR60t7Po%fMcg8Gi",
+			"AbTp9!fok", "Ob&)ZsmKq(QkWCg^2U9-M0N", "q3O!s)Qf+"
+	} )
 	public void validateIsPassword( String password ) {
 		
 		assertTrue( validatorFieldService.isPasswordValid( password ) );
@@ -22,16 +28,15 @@ class FieldsValidationApplicationTests {
 	}
 	
 	@ParameterizedTest
-	@ValueSource( strings = {"x0AA 1_nUVz", "", "S5B#Z4G4!8)D", "PsL-xCaY", "NzXIW-M15M", 
-							 "5+g2!b6N5", "HEa!wuz5!", "zntUX2YGhjjjC#I!", "AO(z)00+Vh4O-oP", 
-							 "Slt7j-(OUA!)+ppt86$3", "K^Dpn@@eE9", "YY5R8Yfw$AW", "hJLhdb6K9CmDnAe", "@0wrmz6q7d9up"} )
+	@ValueSource( strings = {
+			"x0AA 1_nUVz", "", "S5B#Z4G4!8)D", "PsL-xCaY", "NzXIW-M15M",
+			"5+g2!b6N5", "HEa!wuz5!", "zntUX2YGhjjjC#I!", "AO(z)00+Vh4O-oP",
+			"Slt7j-(OUA!)+ppt86$3", "K^Dpn@@eE9", "YY5R8Yfw$AW", "hJLhdb6K9CmDnAe", "@0wrmz6q7d9up"
+	} )
 	public void passowrdIsNotValid( String password ) {
 		
 		assertFalse( validatorFieldService.isPasswordValid( password ) );
 		
 	}
-	
-	@Autowired
-	private ValidatorFieldService validatorFieldService;
 
 }
