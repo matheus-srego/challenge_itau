ad5336b11ea8d926cd806558f5536e34fd577fba
Matheus Silva Rego <matheus.gomes@al.infnet.edu.br>
2024-09-07 14:23:59 -0300

    feat(utils): add phrase in constant

--- FieldsValidation/src/main/java/com/challenge/itidigital/fieldsvalidation/utils/CONSTANTS.java
+++ FieldsValidation/src/main/java/com/challenge/itidigital/fieldsvalidation/utils/CONSTANTS.java
@@ -1,4 +1,7 @@
 package com.challenge.itidigital.fieldsvalidation.utils;
 
 public class CONSTANTS {
+
+    public static final String METHOD_NOT_EXIST = "Could not validate the field with the requested method. Method not found.";
+
 }
